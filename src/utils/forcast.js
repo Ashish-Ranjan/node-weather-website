10c9bf2fbc82520fe4d36d1de3b1d23d1f180a55
shish100@gmail.com <shish100@gmail.com>
2022-01-16 00:33:30 +0000

    Add Humidity

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -11,7 +11,7 @@ const forCast = (latitude, longitude, callback) => {
       callback("Unable to find location", undefined);
     } else {
       const { weather_descriptions, temperature, feelslike, humidity } = body.current;
-      const data = `${weather_descriptions[0]}, It is currently ${temperature} degrees. It feels like ${feelslike} degrees out. current humidity ${humidity}.`;
+      const data = `${weather_descriptions[0]}, It is currently ${temperature} degrees. It feels like ${feelslike} degrees out. current humidity ${humidity}%.`;
       callback(undefined, data);
     }
   });
